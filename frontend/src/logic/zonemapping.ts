7c89277a7a26bed4f8718b549b1d93f564effdf3
SANYA MISHRA <121633614+sanyamishra01@users.noreply.github.com>
2025-12-25 12:03:52 +0530

    Update zonemapping.ts

--- frontend/src/logic/zonemapping.ts
+++ frontend/src/logic/zonemapping.ts
@@ -6,7 +6,7 @@
 export type Zone = "green" | "yellow" | "red";
 
 export function getZone(lhi: number): Zone {
-  if (lhi >= 0.45) return "green";
-  if (lhi >= 0.25) return "yellow";
+  if (1.0 >= lhi >= 0.45) return "green";
+  if (0.45 >= lhi >= 0.25) return "yellow";
   return "red";
 }
